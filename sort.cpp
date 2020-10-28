230fb1e2ba54c0babce036a5e24bf74d331cb4be
schnellheiter <schnelldickheiter@gmail.com>
2020-10-28 22:38:32 +0900

    neapSoatおよびprintVecを追加

--- sort.cpp
+++ sort.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
 
+void printVec(std::vector<int> a);
 void swap(int *a, int *b);
 void bubbleSort(std::vector<int> *a);
 void insertSort(std::vector<int> *a);
+void pushdown(std::vector<int> *a, int first, int last);
+void heapSort(std::vector<int> *a);
 
 
 int main(){
@@ -15,11 +18,8 @@ int main(){
     }
 
     std::cout << std::endl;
-    insertSort(&v);
-    for(int i = 0; i < 10; i++){
-        std::cout << v.at(i) << " ";
-    }
-    std::cout << std::endl;
+    heapSort(&v);
+    printVec(v);
     return 0;
 }
 
@@ -29,6 +29,47 @@ void swap(int *a, int *b){
     *a = *b;
     *b = temp;
     return;
+}
+
+
+void printVec(std::vector<int> a){
+    int size = a.size();
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << a.at(i) << " ";
+    }
+    std::cout << std::endl;
+    
+}
+
+void pushdown(std::vector<int> *a, int first, int last){ //トップダウン式にヒープを作成する
+    int parent = first; //親
+    int child = 2 * parent + 1; //左の子
+    while(child < last){
+        if((child < last-1) && (a->at(child) < a->at(child+1))){
+            child++;
+        }
+        if(a->at(child) <= a->at(parent)){
+            break;
+        }
+        else{
+            swap(&a->at(child), &a->at(parent));
+            parent = child;
+            child = 2 * parent + 1;
+        }
+    }
+}
+
+void heapSort(std::vector<int> *a){
+    int size = a->size();
+    for( int i = (size-1)/2; i >= 0; i--){
+        pushdown(a, i, size);
+
+    }
+    for( int i = size-1; i>= 2; i--){
+        swap(&a->at(0), &a->at(i));
+        pushdown(a, 0, i);
+    }
 }
 
 void bubbleSort(std::vector<int> *a){
